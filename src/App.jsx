95d2c138ff42f3d390b2a93c051ff62f1234cf2d
yan <yan.braverman37@gmail.com>
2024-05-26 10:23:25 +0300

    Add custom theme and AppBar

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,60 @@ import {
 	Route,
 	Navigate,
 } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import MenuIcon from '@mui/icons-material/Menu';
+import AppBar from '@mui/material/AppBar';
+// ==============================
 import './App.css';
 import Albums from './components/albums/Albums';
 import Users from './components/users/Users';
 // ==============================
 import { setActive } from './services/style-service';
-import './App.css'
+import './App.css';
+import { Button } from '@mui/material';
 
 function App() {
-
 	// const setActive = ({isActive}) => isActive ? 'selected' : ''
 	return (
 		<Router>
+			<Box sx={{ flexGrow: 1, width: '900px'}}>
+				<AppBar position='static'>
+					<Toolbar>
+						<IconButton
+							size='large'
+							edge='start'
+							color='inherit'
+							aria-label='open drawer'
+							sx={{ mr: 2 }}
+						>
+							<MenuIcon />
+						</IconButton>
+						<Typography
+							variant='h6'
+							noWrap
+							component='div'
+							fontSize={15}
+							sx={{
+								flexGrow: 1,
+								display: { xs: 'none', sm: 'block' },
+							}}
+						>
+							Albums Manager
+						</Typography>
+						<Button color='inherit'>Login</Button>
+					</Toolbar>
+				</AppBar>
+			</Box>
 			<div className='header'>
 				<ul>
 					{/* <li>
 						<a href='/albums'>Test</a>
 					</li> */}
 					<li>
-						<NavLink 
-							to='/albums'
-							className={setActive}
-							>
+						<NavLink to='/albums' className={setActive}>
 							Albums
 						</NavLink>
 					</li>
@@ -48,12 +80,10 @@ function App() {
 				</ul>
 			</div>
 			<Routes>
-				<Route path='/'>
-					Home
-				</Route>
-				<Route path='/albums/*' element={<Albums />}/>
+				<Route path='/'>Home</Route>
+				<Route path='/albums/*' element={<Albums />} />
 				<Route path='/users/*' element={<Users />} />
-				<Route path='*' element={<Navigate to='/'/>} />
+				<Route path='*' element={<Navigate to='/' />} />
 			</Routes>
 		</Router>
 	);
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from '@mui/material/styles';
+import {createTheme} from '@mui/material';
 // =============================
 import App from './App.jsx';
 import store from './store';
 // =============================
 import './index.css';
 
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#D10808',
+			light: '#D6C8E1',
+			dark: '#82A2B5'
+		}
+	},
+	typography: {
+		fontSize: 30
+	}
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<ThemeProvider theme={theme}>
+				<App />
+			</ThemeProvider>
 		</Provider>
 	</React.StrictMode>
 );
